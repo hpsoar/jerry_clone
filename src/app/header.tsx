3c69104757b5fe119113e25b4b5dd902fdef2dcd
Huang Peng <huangpeng@chunyu.me>
2024-04-29 14:26:26 +0800

    MOD: ..

---
+++ src/app/header.tsx
@@ -0,0 +1,10 @@
+
+export default function Header() {
+  return (
+      <div class="sc-cede6ad-0 fiUclQ sc-38492363-0 kAJvHO full-hero">
+      <span >
+      <img alt="background" sizes="100vw" srcset="https://getjerry.com/_next/image?url=https%3A%2F%2Fgetjerry.com%2F_next%2Fstatic%2Fimages%2FdefaultHeroCta-7e495d6bb88b334c..jpg&w=640&q=90 640w, https://getjerry.com/_next/image?url=https%3A%2F%2Fgetjerry.com%2F_next%2Fstatic%2Fimages%2FdefaultHeroCta-7e495d6bb88b334c..jpg&w=750&q=90 750w, https://getjerry.com/_next/image?url=https%3A%2F%2Fgetjerry.com%2F_next%2Fstatic%2Fimages%2FdefaultHeroCta-7e495d6bb88b334c..jpg&w=828&q=90 828w, https://getjerry.com/_next/image?url=https%3A%2F%2Fgetjerry.com%2F_next%2Fstatic%2Fimages%2FdefaultHeroCta-7e495d6bb88b334c..jpg&w=1080&q=90 1080w, https://getjerry.com/_next/image?url=https%3A%2F%2Fgetjerry.com%2F_next%2Fstatic%2Fimages%2FdefaultHeroCta-7e495d6bb88b334c..jpg&w=1200&q=90 1200w, https://getjerry.com/_next/image?url=https%3A%2F%2Fgetjerry.com%2F_next%2Fstatic%2Fimages%2FdefaultHeroCta-7e495d6bb88b334c..jpg&w=1920&q=90 1920w, https://getjerry.com/_next/image?url=https%3A%2F%2Fgetjerry.com%2F_next%2Fstatic%2Fimages%2FdefaultHeroCta-7e495d6bb88b334c..jpg&w=2048&q=90 2048w, https://getjerry.com/_next/image?url=https%3A%2F%2Fgetjerry.com%2F_next%2Fstatic%2Fimages%2FdefaultHeroCta-7e495d6bb88b334c..jpg&w=3840&q=90 3840w" src="https://getjerry.com/_next/image?url=https%3A%2F%2Fgetjerry.com%2F_next%2Fstatic%2Fimages%2FdefaultHeroCta-7e495d6bb88b334c..jpg&w=3840&q=90" decoding="async" data-nimg="fill" class="background-img"/>
+      </span>
+    </div>
+      );
+}
